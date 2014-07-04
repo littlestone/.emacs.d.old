01b68a67d2a6c40457a5f7816fbf03a052740ab4
littlestone <razorsniper@gmail.com>
2014-07-04 13:34:47 -0400

    Zen of Emacs

--- configs/appearance.el
+++ configs/appearance.el
@@ -40,7 +40,7 @@
 
 ;; Windows下Emacs中文字体设置
 (if (eq system-type 'gnu/linux)
-    (set-default-font "Consolas-14")
+    (set-default-font "Consolas-13")
   (set-default-font "Consolas-11"))
 (set-fontset-font "fontset-default" 'gb18030 '("Microsoft YaHei" . "unicode-bmp")) ; 中文使用微软雅黑字体
 
