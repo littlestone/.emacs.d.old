1f0ed1dbff539306b19353a3cf21b2e23a0a79fc
littlestone <razorsniper@gmail.com>
2014-10-31 18:24:25 -0400

    Zen of Emacs

---
+++ configs/init-evil.el
@@ -0,0 +1,65 @@
+;;; ====================================
+;;; + An extensible vi layer for Emacs +
+;;; ====================================
+
+(require 'evil)
+
+(evil-mode 1)
+
+;; I want the Emacs keybinding to work in Evil
+(define-key evil-normal-state-map "\C-r" 'isearch-backward-regexp)
+(define-key evil-normal-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-motion-state-map "\C-e" 'evil-end-of-line)
+
+;; Don't wait for any other keys after escape is pressed.
+(setq evil-esc-delay 0)
+
+;; Set evil-shift-width to 2 space
+(setq evil-shift-width 2)
+
+;; Exit insert mode by pressing j and then j quickly
+(key-chord-mode 1)
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+
+;; Make sure escape gets back to normal state and quits things.
+(define-key evil-insert-state-map [escape] 'evil-normal-state)
+(define-key evil-visual-state-map [escape] 'evil-normal-state)
+(define-key evil-emacs-state-map [escape] 'evil-normal-state)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-ns-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-completion-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-must-match-map [escape] 'abort-recursive-edit)
+(define-key minibuffer-local-isearch-map [escape] 'abort-recursive-edit)
+
+;; Change cursor in different modes.
+(setq evil-default-cursor 'box)
+(setq evil-normal-state-cursor 'box)
+(setq evil-visual-state-cursor 'hollow)
+(setq evil-replace-state-cursor 'hbar)
+
+;; Set the initial evil state that certain major modes will be in.
+(evil-set-initial-state 'org-mode 'normal)
+(evil-set-initial-state 'dired-mode 'normal)
+(evil-set-initial-state 'ibuffer-mode 'normal)
+(evil-set-initial-state 'fundamental-mode 'normal)
+(evil-set-initial-state 'cider-repl-mode 'normal)
+(evil-set-initial-state 'slime-repl-mode 'normal)
+(evil-set-initial-state 'inferior-emacs-lisp-mode 'normal)
+(evil-set-initial-state 'package-menu-mode 'emacs)
+(evil-set-initial-state 'browse-kill-ring-mode 'emacs)
+(evil-set-initial-state 'recentf-dialog-mode 'emacs)
+(evil-set-initial-state 'magit-log-edit-mode 'emacs)
+(evil-set-initial-state 'eshell-mode 'emacs)
+(evil-set-initial-state 'sldb-mode 'emacs)
+(evil-set-initial-state 'grep-mode 'emacs)
+(evil-set-initial-state 'magit-mode 'emacs)
+(evil-set-initial-state 'git-commit-mode 'emacs)
+(evil-set-initial-state 'magit-status-mode 'emacs)
+
+(provide 'init-evil)
